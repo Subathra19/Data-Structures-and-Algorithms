4ca43cf65df7c7c9d9017cf6fec83b7edaad60a2
Sri Subathra Devi B <subathrad18@gmail.com>
2021-06-24 19:43:00 +0530

    Create rainwater.c

---
+++ Exercises/Additional-Exercise/rainwater.c
@@ -0,0 +1,34 @@
+//Rain water trapping
+#include <stdio.h>
+int rainwaterTrapping(int a[],int n){
+    int lmax=0,rmax=0;
+    int l=0,h=n-1;
+    int res=0;
+    while(l<=h){
+        if(a[l]<a[h]){
+            if(a[l]>lmax){
+                lmax=a[l];
+            }else{
+              res=res+lmax-a[l];  
+            }
+            l++;
+        }else{
+            if(a[h]>rmax){
+                rmax=a[h];
+            }else{
+                res=res+rmax-a[h];
+            }
+            h--;
+        }
+    }
+    return res;
+}
+int main()
+{
+    int k;
+    int a[]={3,0,0,2,0,4};
+    int n=sizeof(a)/sizeof(a[0]);
+    int r=rainwaterTrapping(a,n);
+    printf("Maximum water that can be accumulated is: %d",r);
+}
+
