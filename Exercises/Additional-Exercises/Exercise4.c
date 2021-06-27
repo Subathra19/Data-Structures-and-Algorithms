fe919f6b2e89336e5e95f0feff5502ca33d69af0
Sri Subathra Devi B <subathrad18@gmail.com>
2021-06-27 15:17:54 +0530

    Create Exercise4.c

---
+++ Exercises/Additional-Exercises/Exercise4.c
@@ -0,0 +1,23 @@
+//Copy set bits in a range
+#include <stdio.h>
+void copySetBit(int x,int y,int l,int r){
+    if(l<1 ||r>32){
+        return;
+    }
+    for(int i=l;i<=r;i++){
+        int set=1<<(i-1);
+        //check whether that bit is set in y
+        if(y&set){
+            //if it it set, then set that bit in x
+            x=x|set;
+        }
+    }
+    printf("\nModified x: %d",x);
+}
+int main(){
+    int x=8,y=7,l=1,r=2;
+    printf("x: %d",x);
+    copySetBit(x,y,l,r);
+    return 0;
+}
+c
