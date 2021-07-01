67cf1fda2405211508c68d474d03322496d2030a
Sri Subathra Devi B <subathrad18@gmail.com>
2021-07-01 15:26:38 +0530

    Create Exercise27.c

---
+++ Exercises/Array/Exercise27.c
@@ -0,0 +1,26 @@
+//Convert Decimal number to roman numeral
+#include <stdio.h>
+#define NUMBER_OF_STRING 13
+#define MAX_STRING_SIZE 3
+void roman(int n){
+    int num[] = {1,4,5,9,10,40,50,90,100,400,500,900,1000}; 
+    char s[NUMBER_OF_STRING][MAX_STRING_SIZE] = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+    int i=12;// total number of symbols
+    while(n>0){
+        int rem=n/num[i];
+        n=n%num[i];
+        while(rem>0){
+            printf("%s",s[i]);
+            rem--;
+        }
+        i--;
+    }
+}
+int main()
+{
+    int n;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    roman(n);
+    return 0;
+}
